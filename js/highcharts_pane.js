ae1fa2d990e7d68fcb80e860c5529d346164b9e3
Cale Bierman <cale-bierman@uiowa.edu>
2014-06-27 09:47:36 -0500

    first commit

---
+++ js/highcharts_pane.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Javascript for the highcharts pane module.
+ */
+
+ // Namespace jQuery to avoid conflicts.
+(function($) {
+  	//Define the behavior.
+  	Drupal.behaviors.highchartsPane = {
+  		attach: function(context, settings) {
+  			var chartID = "#" + Drupal.settings.highchartsPane.chartID;
+        console.log(Drupal.settings.highchartsPane);
+        $(chartID, context).once("highchartsPane", function() {
+          var chart = new Highcharts.Chart(Drupal.settings.highchartsPane.chartData);
+  			});
+  		}
+  	};
+})(jQuery);
